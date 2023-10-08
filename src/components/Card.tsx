780bce22d765123add24a67e221779c3c9b42f6e
Kenny Sabangan <ksabangan121@gmail.com>
2023-10-07 23:31:40 -0700

    Optimize app before install PWA files

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,8 +8,8 @@ const Card = (props: CardProps) => {
     const { id, name, email } = props;
 
     return (
-        <div className="bg-teal-300 inline-block rounded-lg p-1 m-2 transform scale-100 transition-all hover:scale-105 drop-shadow-xl">
-            <img alt='robot' width="250px" src={`https://robohash.org/${id}?300x300`} draggable='false' />
+        <div className="bg-teal-300 inline-block rounded-lg p-2 m-2 transform scale-100 transition-all hover:scale-105 drop-shadow-xl">
+            <img alt='robot' width="240px" height="300px" src={`https://robohash.org/${id}?240x300`} draggable='false' />
             <div className="flex flex-col items-center my-4">
                 <h2 className="text-lg font-bold">{name}</h2>
                 <p>{email}</p>
